9afefe8a6d9a54a6cc7b4a2292707e1a6d1fd0a3
kkasg38-stack <kkasg38@gmail.com>
2026-08-27 17:39:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import ClientBlockListener from "./components/ClientBlockListener";
 import { getCurrentUserId } from "./utils/orderId";
 
 // export const api_route = "http://localhost:8080";
-export const api_route = "https://test-ser-production.up.railway.app";
+export const api_route = "https://tmn-kse-production.up.railway.app";
 export const socket = io(api_route);
 
 export function getKeysWithTrueValue(obj) {
